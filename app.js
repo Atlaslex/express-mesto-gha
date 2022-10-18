eb026f6069a83bd4df8650e7fefa25a287900f46
Atlaslex <atlaslex@yandex.ru>
2022-10-19 11:28:50 +1200

    13_2

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '634f3431a9fe1ac62ed8b849'
+  };
+
+  next();
 });
+
+app.use('/', userRouter);
+app.use('/', cardRouter);
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  });
---
+++ errors/Errors.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-classes-per-file */
+class ValidationErrors extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ValidationError';
+    this.statusCode = 400;
+  }
+}
+
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NotFound';
+    this.statusCode = 404;
+  }
+}
+
+module.exports = { ValidationErrors, NotFoundError };
