79ba17592f5efe83ecc73c474bd71ff81bb5f9e4
Atlaslex <atlaslex@yandex.ru>
2022-10-24 15:38:39 +1200

    14_4

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const NotFoundError = require('./utils/errorcodes/not-found-error');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-// mongoose.connect('mongodb://localhost:3000/mestodb');
+mongoose.connect('mongodb://localhost:3000/mestodb');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
