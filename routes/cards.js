9b5c60cf6555939a897ad307d32bab5104308a7e
Atlaslex <atlaslex@yandex.ru>
2022-10-24 14:46:52 +1200

    14_3

--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { log } = require('../middlewares/consolelog');
-const { LinksRegExp, IdRegExp } = require('../utils/all-reg-exp');
+const { LinksRegExp } = require('../utils/all-reg-exp');
 
 const {
   getCards,
@@ -22,7 +22,7 @@ router.post('/', celebrate({
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().pattern(IdRegExp).length(24),
+    cardId: Joi.string().hex().length(24),
   }),
 }), deleteCard);
 
